b1a9b5ad253f40b5fae61b0aeab4356758ad4ba6
Henry Kam <h3nrykam@gmail.com>
2021-10-23 16:11:17 -0700

    add sample seeds

---
+++ db/query.sql
@@ -0,0 +1,7 @@
+-- SELECT *
+-- FROM employee
+-- JOIN roles ON employee.role_id = roles.id;
+
+-- SELECT *
+-- FROM department
+-- JOIN roles ON department.id = roles.department_id;
--- db/schema.sql
+++ db/schema.sql
@@ -8,12 +8,12 @@ CREATE TABLE department(
     department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id);
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
@@ -22,6 +22,6 @@ CREATE TABLE employee(
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id);
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
